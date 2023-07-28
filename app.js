c1414b1dc5627b67a17b57604643327f0221cb8e
Đặng Trần Tiến <dangtrantien98@gmail.com>
2023-07-28 10:41:06 +0700

    update

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res, next) => {
-  res.send('Server side');
-});
-
 app.use('/api/movies', movieRoutes);
-
-app.get('/404', (req, res, next) => {
-  res.status(404).send('Route not found');
-});
 
 app.listen(process.env.PORT || 5000);
