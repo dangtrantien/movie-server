17fa87ca355f0b7f8ee7c19bb5219a948a36cfc8
Đặng Trần Tiến <dangtrantien98@gmail.com>
2023-07-28 16:45:53 +0700

    update deployment

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const helmet = require('helmet');
-// const compression = require('compression');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const movieRoutes = require('./routes/movie');
 
@@ -15,8 +15,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(helmet());
-// app.use(compression());
+app.use(helmet());
+app.use(compression());
 
 app.use('/api/movies', movieRoutes);
 
